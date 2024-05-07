aed550df3eee0423666fcd67d4a05f96468ee82f
kotek <scpktk@gmail.com>
2024-05-07 22:10:33 +0900

    Second commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+</script>
+
+<template>
+  <h1>ラーメン美味しいよね</h1>
+  <dev class="main-page-root">
+    <form>
+      <div>
+        種類(こってり？あっさり？)
+        <input type="text" />
+      </div>
+
+      <div>
+        サイズ(大・中・小)
+        <input type="text" />
+      </div>
+
+      <div>
+        スープの種類
+        <input type="text" />
+      </div>
+      <button type="submit">送信！！！！</button>
+    </form>
+  </dev>
+</template>
+
+<style scoped>
+  .read-the-docs {
+    color: #888;
+  }
+</style>

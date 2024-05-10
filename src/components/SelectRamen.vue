a9521d81925872e29d4b5de20f71b5a76b42b2f1
yuzee <ym.kyoto.306@gmail.com>
2024-05-11 00:26:33 +0900

    SelectRamen.vueを作成

---
+++ src/components/SelectRamen.vue
@@ -0,0 +1,159 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="page">
+
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-box">
+            <div class="ramen_name">
+                <p>ラーメン</p>
+            </div>
+            <div class="inner-container">
+                <div class="calorie">
+                    <p>100 kcal</p>
+                </div>
+                
+                <div class="date">
+                    <p>2024/05/10</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </template>
+
+<style scoped>
+p {
+    margin: 5px;
+}
+.ramen_name {
+    text-align: center;
+    font-size: 32px;
+    margin: 0;
+    font-weight: bold;
+}
+
+.calorie {
+    text-align: center;
+}
+
+.date {
+    text-align: center;
+}
+
+.inner-container {
+    display: flex;
+    justify-content: space-between;
+    color: rgb(76, 76, 76);
+}
+
+.container-box{
+    background: #f4f4f4;
+    padding: 16px;
+    border-radius: 16px;
+    margin: 10px 10px 20px 10px;
+    box-shadow: 8px 8px 8px rgba(34, 34, 34, 0.3);
+}
+
+.page{
+    overflow-y: scroll;
+    background: #f29b5d;
+}
+</style>

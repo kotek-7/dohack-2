c87f4eceeb8a788232ed45865fde6aa60947a233
kotek <scpktk@gmail.com>
2024-05-11 14:55:17 +0900

    Modified eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,18 @@
+// @ts-check
+import js from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { parser, parserOptions } from "eslint-plugin-vue/lib/configs/base";
+
 export default [
+  js.configs.recommended,
   ...pluginVue.configs["flat/recommended"],
   {
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
+    rules: {},
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      sourceType: "module",
     },
   },
+  eslintConfigPrettier,
 ];
